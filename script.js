120e7343442835f26e853725d00767f4fbb4d8e8
NuoBH <36141903+NuoBH@users.noreply.github.com>
2024-06-25 01:14:37 -0700

    web

---
+++ script.js
@@ -0,0 +1,57 @@
+let header;
+let links;
+
+document.addEventListener("DOMContentLoaded", ()=>{
+    header = document.getElementById("zhuHeader");
+    centerHeader();
+
+    links = document.getElementsByClassName("navLink");
+
+    for(let i = 0; i < links.length; i++){
+        let link = links[i];
+        randomizeMaskPos(link);
+        link.addEventListener("mouseover", e=>{
+            randomizeMaskPos(e.target);
+        });
+    }
+})
+
+function centerHeader(){
+    let headerHeight = header.offsetHeight;
+
+    if(headerHeight < window.innerHeight && window.innerWidth >= 1100){
+        let marginTop;
+        marginTop = (window.innerHeight - headerHeight) / 2;
+        header.style.marginTop = `${marginTop * 0.825}px`;
+    }
+    else{
+        if(window.innerWidth >= 1100){
+            header.style.marginTop = '7%';
+        }
+        else{
+            if(headerHeight < window.innerHeight * 0.85){
+                let marginTop;
+                marginTop = (window.innerHeight - headerHeight) / 2;
+                header.style.marginTop = `${marginTop * 0.8}px`;
+            }
+            else{
+                header.style.marginTop = '10%';
+            }
+        }
+    }
+}
+
+function randomizeMaskPos(link){
+        let randomX = randomIntFromInterval(25, 35);
+        let randomY = randomIntFromInterval(2, 7);
+        link.style.setProperty("--randomPosX", `${randomX}px`);
+        link.style.setProperty("--randomPosY", `${randomY}px`);
+}
+
+function randomIntFromInterval(min, max) { // min and max included 
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
+window.onresize = ()=>{
+    centerHeader();
+}
