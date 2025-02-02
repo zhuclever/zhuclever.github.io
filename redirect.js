722d6db718030e9b30d8a5108ea5bac4b15481f5
NuoBH <36141903+NuoBH@users.noreply.github.com>
2025-02-02 10:36:06 -0800

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -2,4 +2,4 @@
 const currentQuery = encodeURIComponent(window.location.search);
 console.log(window.location.search);
 // Redirect with existing parameters
-window.location.href = `https://zhua05nuo.wixstudio.com/zhuclever${currentQuery}`;
+//window.location.href = `https://zhua05nuo.wixstudio.com/zhuclever${currentQuery}`;
