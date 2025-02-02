c11132f0e8895e9cc645b86e764bb37d34556ae6
NuoBH <36141903+NuoBH@users.noreply.github.com>
2025-02-02 10:40:19 -0800

    Update redirect.js

--- redirect.js
+++ redirect.js
@@ -1,5 +1,5 @@
 // Get current query parameters (e.g., "?source=google&id=123")
-const currentQuery = encodeURIComponent(window.location.search);
-console.log(window.location.search);
+const currentQuery = window.location.search;
+
 // Redirect with existing parameters
-//window.location.href = `https://zhua05nuo.wixstudio.com/zhuclever${currentQuery}`;
+window.location.href = `https://zhua05nuo.wixstudio.com/zhuclever${currentQuery}`;
