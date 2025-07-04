ceb951543e2c40d36fcd03d506638d4b791068c6
0buo <zhuang0buo@gmail.com>
2025-07-04 04:53:57 -0700

    added digital double open call

---
+++ digital-double/script.js
@@ -0,0 +1,58 @@
+let header;
+let links;
+
+const resizeObserver = new ResizeObserver(()=>{
+    centerHeader();
+})
+
+window.addEventListener("load", ()=>{
+    header = document.getElementById("zhuHeader");
+    centerHeader();
+    resizeObserver.observe(document.querySelector("#zhuHeader"));
+
+    links = document.getElementsByClassName("navLink");
+
+    for(let i = 0; i < links.length; i++){
+        let link = links[i];
+        randomizeMaskPos(link);
+        link.addEventListener("mouseover", e=>{
+            randomizeMaskPos(e.target);
+        });
+    }
+})
+
+function centerHeader(){
+    let headerHeight = header.offsetHeight;
+
+    if(headerHeight < window.innerHeight && window.innerWidth >= 1100){
+        let marginTop;
+        marginTop = (window.innerHeight - headerHeight) / 2;
+        header.style.marginTop = `${marginTop * 0.825}px`;
+    }
+    else{
+        if(window.innerWidth >= 1100){
+            header.style.marginTop = '7%';
+        }
+        else{
+            if(headerHeight < window.innerHeight * 0.85){
+                let marginTop;
+                marginTop = (window.innerHeight - headerHeight) / 2;
+                header.style.marginTop = `${marginTop * 0.8}px`;
+            }
+            else{
+                header.style.marginTop = '10%';
+            }
+        }
+    }
+}
+
+function randomizeMaskPos(link){
+        let randomX = randomIntFromInterval(50, 100);
+        let randomY = randomIntFromInterval(1, 3);
+        link.style.setProperty("--randomPosX", `${randomX}px`);
+        link.style.setProperty("--randomPosY", `${randomY}px`);
+}
+
+function randomIntFromInterval(min, max) { // min and max included 
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
